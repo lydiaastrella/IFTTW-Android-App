8fd050a0448ece9898dfd8c9ab514e29b6d810de
Lydia Astrella <lydiaastrella@gmail.com>
2020-03-13 22:46:20 +0700

    fixing bug, tidy up class code

--- app/src/main/java/com/example/ifttw/openweather/JSONWeatherParser.java
+++ app/src/main/java/com/example/ifttw/openweather/JSONWeatherParser.java
@@ -15,14 +15,9 @@ public class JSONWeatherParser {
         // We start extracting the info
         Location loc = new Location();
 
-        JSONObject coordObj = getObject("coord", jObj);
-        loc.setLatitude(getFloat("lat", coordObj));
-        loc.setLongitude(getFloat("lon", coordObj));
-
         JSONObject sysObj = getObject("sys", jObj);
         loc.setCountry(getString("country", sysObj));
         loc.setSunrise(getInt("sunrise", sysObj));
-        loc.setSunset(getInt("sunset", sysObj));
         loc.setCity(getString("name", jObj));
         weather.location = loc;
 
@@ -31,29 +26,8 @@ public class JSONWeatherParser {
 
         // We use only the first value
         JSONObject JSONWeather = jArr.getJSONObject(0);
-        weather.currentCondition.setWeatherId(getInt("id", JSONWeather));
         weather.currentCondition.setDescr(getString("description", JSONWeather));
         weather.currentCondition.setCondition(getString("main", JSONWeather));
-        weather.currentCondition.setIcon(getString("icon", JSONWeather));
-
-        JSONObject mainObj = getObject("main", jObj);
-        weather.currentCondition.setHumidity(getInt("humidity", mainObj));
-        weather.currentCondition.setPressure(getInt("pressure", mainObj));
-        weather.temperature.setMaxTemp(getFloat("temp_max", mainObj));
-        weather.temperature.setMinTemp(getFloat("temp_min", mainObj));
-        weather.temperature.setTemp(getFloat("temp", mainObj));
-
-        // Wind
-        JSONObject wObj = getObject("wind", jObj);
-        weather.wind.setSpeed(getFloat("speed", wObj));
-        weather.wind.setDeg(getFloat("deg", wObj));
-
-        // Clouds
-        JSONObject cObj = getObject("clouds", jObj);
-        weather.clouds.setPerc(getInt("all", cObj));
-
-        // We download the icon to show
-
 
         return weather;
     }
@@ -66,10 +40,6 @@ public class JSONWeatherParser {
 
     private static String getString(String tagName, JSONObject jObj) throws JSONException {
         return jObj.getString(tagName);
-    }
-
-    private static float  getFloat(String tagName, JSONObject jObj) throws JSONException {
-        return (float) jObj.getDouble(tagName);
     }
 
     private static int  getInt(String tagName, JSONObject jObj) throws JSONException {
